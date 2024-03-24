57567d4b3320dd7aa95ef1d38e143ac1eb2ca217
Jalal Assaly <jalal.assaly@gmail.com>
2024-03-25 01:08:37 +0200

    Add format for datetime values

--- src/main/java/org/example/accesscontrolapi/exceptionhandler/responsebodies/WebClientResponseExceptionBody.java
+++ src/main/java/org/example/accesscontrolapi/exceptionhandler/responsebodies/WebClientResponseExceptionBody.java
@@ -1,6 +1,7 @@
 package org.example.accesscontrolapi.exceptionhandler.responsebodies;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -12,31 +13,26 @@ import lombok.Setter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class WebClientResponseExceptionBody {
     @JsonProperty("timestamp")
-    private  String dateTime;
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    private String dateTime;
     @JsonProperty("status")
-    private  String status;
+    private String status;
     @JsonProperty("message")
-    private  String errorMessages;
+    private String errorMessages;
 
     @JsonIgnore
     String exception;
 
-    public WebClientResponseExceptionBody(String exception){
+    public WebClientResponseExceptionBody(String exception) {
         this.exception = exception;
         formatException(this.exception);
     }
 
-    private void formatException(String exception){
-
-        String exceptionFormated = exception.replace("{", "").replace("}", "").replace("\"", "");
-        String[] parts = exceptionFormated.split(",");
-        this.dateTime= exceptionFormated.substring(10,29);
+    private void formatException(String exception) {
+        String exceptionFormatted = exception.replace("{", "").replace("}", "").replace("\"", "");
+        String[] parts = exceptionFormatted.split(",");
+        this.dateTime = exceptionFormatted.substring(10, 29);
         this.status = parts[1].split(":")[1].trim();
         this.errorMessages = parts[2].split(":")[1].trim();
-
     }
-
-
-
-
 }
