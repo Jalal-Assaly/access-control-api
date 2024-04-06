1287a3cb7b16a203148cb1babb5fa336ac34161c
Ahmed Shalabi <shtk3eb01@gmail.com>
2024-04-07 00:06:21 +0200

    Code refactor

--- src/main/java/org/pacs/accesscontrolapi/exceptionhandler/customexceptions/AttributesMismatchException.java
+++
@@ -1,7 +0,0 @@
-package org.pacs.accesscontrolapi.exceptionhandler.customexceptions;
-
-public class AttributesMismatchException extends RuntimeException {
-    public AttributesMismatchException(String message) {
-        super(message);
-    }
-}
